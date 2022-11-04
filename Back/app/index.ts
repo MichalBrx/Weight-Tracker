e3784f3302fa772ab31a0b147dc5c0dead547fb6
MichalBrx <michalhaj2003@gmail.com>
2022-11-04 20:46:16 +0100

    Cleaning Code

--- Back/app/index.ts
+++ Back/app/index.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 import express from 'express'
+import {Request, Response, NextFunction, Errback} from 'express'
 
 const app = express()
 require('dotenv').config()
@@ -10,6 +11,25 @@ const jwt = require('jsonwebtoken')
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+
+
+interface newUser {
+    name: string,
+    email: string,
+    password: string
+    
+}
+
+declare global {
+    namespace Express {
+      interface Request {
+        user: newUser;
+      }
+    }
+  }
+
+//    IT should be saved in Cookies or localStorage
+  let refreshTokens = [] 
 
 app.post('/register', async (req,res) => {
 
@@ -25,8 +45,10 @@ app.post('/register', async (req,res) => {
                 password: hashedPassword
             }
         })
-        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-        res.status(200).json({ user:user, accessToken: accessToken })
+        const accessToken = generateAccessToken(user)
+        const refreshToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+        refreshTokens.push(refreshToken)
+        res.status(200).json({ user:user, accessToken: accessToken, refreshToken: refreshToken })
     }
     catch{
         if( name == '' || email == '' || password == '' ) {
@@ -36,16 +58,80 @@ app.post('/register', async (req,res) => {
         }
 
     }
+})
+
+app.post('/login', async (req:any, res: Response) => {
+    const user = await get_user_by_email(req.body.email)
+    // const password = await bcrypt.compare(req.user.password, isUser.password)
+
+    console.log( user)
+    if (user === null) {
+        console.log("test1")
+        return res.status(400).send('Cannot find user')
+      }
+
+      try {
+        if(await bcrypt.compare(req.body.password, user.password)) {
+            console.log("test2")
+            const accessToken = generateAccessToken(user)
+            const refreshToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+            refreshTokens.push(refreshToken)
+            res.status(200).json({ user:user, accessToken: accessToken, refreshToken: refreshToken })
+        } else {
+            console.log("test3")
+          res.send('Not Allowed')
+        }
+      } catch {
+        console.log("test4")
+        res.status(500).send()
+      }
 
 })
 
-
-
-app.get('/users', async (req, res) => {
-
-    const user = await prisma.user.findMany()
-    res.json(user)
+app.post('/token', (req, res: Response) => {
+    const refreshToken = req.headers.authorization
+    const token = refreshToken && refreshToken.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
+    if (!token.includes(token)) return res.sendStatus(403)
+    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err: any, user:newUser) => {
+        console.log(token)
+        if(err) return res.sendStatus(403)
+        const accessToken = generateAccessToken({ name: user.name})
+        res.json({ accessToken: accessToken })
+    })
 })
+
+app.get('/users',authenticateToken, async (req, res) => {
+
+    const users = await prisma.user.findMany()
+    res.json(users.filter(user => user.email === req.user.email))
+})
+
+async function get_user_by_email(user_email:string){
+    const users = await prisma.user.findMany()
+    const user = users.filter(user => user.email === user_email)
+    return user[0]
+}
+// async function validatePassword(user_password:string) {
+
+// }
+
+function generateAccessToken(user: any) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '3000s'})
+}
+
+function authenticateToken(req:Request, res:Response, next:NextFunction) {
+    const authHeader = req.headers.authorization
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null)
+    return res.status(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err:any, user:newUser) => {
+        if (err) return res.status(403)
+        req.user = user
+        next()
+    })
+}
 
 
 const server = app.listen(3000, () =>
--- Back/server.js
+++ Back/server.js
@@ -34,23 +34,17 @@ app.post('/login', async (req, res) => {
 
 app.post('/register', async (req,res) => {
 
-
-
     try {
 
         const salt = await bcrypt.genSalt()
         const hashedPassword = await bcrypt.hash(req.body.password, salt)
         const newUser = {name: req.body.name, password: hashedPassword, email: req.body.email} 
 
-
-
         prisma.main(newUser).then(user => { res.status(200).json(user)})
     }
     catch {
         res.status(500).json({message: "cannot add new User"})
     }
-    
-
 })
 
 app.listen(3000)
